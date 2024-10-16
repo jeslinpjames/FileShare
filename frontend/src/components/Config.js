93b5116b8f77c34502adb6d18761f6a5794aac58
Nazal-nihad <nazalnihad17@gmail.com>
2024-10-17 04:06:25 +0530

    server ip change:

--- frontend/src/components/Config.js
+++ frontend/src/components/Config.js
@@ -1,4 +1,4 @@
-// const SERVER_IP = 'http://13.201.186.207'; 
-const SERVER_IP = 'http://192.168.11.23:5000';
+const SERVER_IP = 'http://13.201.186.207'; 
+// const SERVER_IP = 'http://192.168.11.23:5000';
 
 export default SERVER_IP;
