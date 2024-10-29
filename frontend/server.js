bfe37a958a0619cbd6403833ea8adbee8319daca
Nazal-nihad <nazalnihad17@gmail.com>
2024-10-29 22:08:10 +0530

    p2p setup

---
+++ frontend/server.js
@@ -0,0 +1,33 @@
+const { PeerServer } = require('peer');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+const localIP = '192.168.68.3'; 
+// Create a PeerJS server
+const peerServer = PeerServer({
+    port: 9000,
+    host: localIP,
+    path: '/myapp',
+    proxied: false,
+    debug: true,
+    allow_discovery: true,
+});
+
+// Start Express server
+const expressServer = app.listen(5000, () => {
+    console.log('Express server listening on port 5000');
+});
+
+peerServer.on('connection', (client) => {
+    console.log('Client connected:', client.id);
+});
+
+peerServer.on('disconnect', (client) => {
+    console.log('Client disconnected:', client.id);
+});
+
+console.log('PeerJS server listening on port 9000');
