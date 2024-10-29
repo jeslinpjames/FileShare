c96e3e19ee5d283eec3c88176065b93f845f8df1
Nazal-nihad <nazalnihad17@gmail.com>
2024-10-29 23:18:46 +0530

    config change

--- frontend/src/components/Config.js
+++ frontend/src/components/Config.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-// const SERVER_IP = 'http://13.201.186.207'; 
-const SERVER_IP = 'http://192.168.68.3:5000';
-=======
 const SERVER_IP = 'https://sharemore.online/api'; 
 // const SERVER_IP = 'http://192.168.11.23:5000';
->>>>>>> 539e0be81501cf762092ce172ef9f4ee90766f6f
 
 export default SERVER_IP;
