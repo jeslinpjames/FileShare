ce0f1757b1ba4d0e77b3c7ad23dfe368bdd1a8e9
Nazal-nihad <nazalnihad17@gmail.com>
2024-11-07 03:42:07 +0530

    local room users dictonary

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # Store active transfers
 active_transfers = {}
 current_video = {}
+room_users = {}
 
 
 def generate_random_code(length=6):
@@ -67,20 +68,101 @@ def download_file():
     return jsonify({'error': 'Invalid code or the transfer has expired'}), 400
 
 
+# @socketio.on('join_room')
+# def handle_join_room(data):
+#     room = data['room']
+#     join_room(room)
+#     # Emit the current video URL to the new user if a video is already loaded
+#     if room in current_video:
+#         emit('video_event', {'type': 'url', 'url': current_video[room]}, room=request.sid)
+#     emit('user_joined', {'user': request.sid}, room=room)
+
 @socketio.on('join_room')
 def handle_join_room(data):
     room = data['room']
+    username = data.get('username', 'Anonymous')
+    
+    if room not in room_users:
+        room_users[room] = {}
+    
+    room_users[room][request.sid] = username
     join_room(room)
-    # Emit the current video URL to the new user if a video is already loaded
+    
+    # Emit current video URL and user list to new user
     if room in current_video:
         emit('video_event', {'type': 'url', 'url': current_video[room]}, room=request.sid)
-    emit('user_joined', {'user': request.sid}, room=room)
+    
+    # Notify others about new user
+    emit('chat_message', {
+        'type': 'system',
+        'content': f'{username} joined the room'
+    }, room=room)
+    
+    # Send updated user list to all in room
+    emit('user_list_update', {
+        'users': list(room_users[room].values())
+    }, room=room)
+
+# @socketio.on('leave_room')
+# def handle_leave_room(data):
+#     room = data['room']
+#     leave_room(room)
+#     emit('user_left', {'user': request.sid}, room=room)
 
 @socketio.on('leave_room')
 def handle_leave_room(data):
     room = data['room']
+    username = room_users[room].get(request.sid, 'Anonymous')
+    
+    if room in room_users and request.sid in room_users[room]:
+        del room_users[room][request.sid]
+        if not room_users[room]:  # If room is empty
+            del room_users[room]
+    
     leave_room(room)
-    emit('user_left', {'user': request.sid}, room=room)
+    
+    # Notify others about user leaving
+    emit('chat_message', {
+        'type': 'system',
+        'content': f'{username} left the room'
+    }, room=room)
+    
+    # Send updated user list
+    if room in room_users:
+        emit('user_list_update', {
+            'users': list(room_users[room].values())
+        }, room=room)
+
+@socketio.on('chat_message')
+def handle_chat_message(data):
+    room = data['room']
+    username = room_users[room].get(request.sid, 'Anonymous')
+    
+    emit('chat_message', {
+        'type': 'user',
+        'username': username,
+        'content': data['message'],
+        'timestamp': data.get('timestamp')
+    }, room=room)
+
+@socketio.on('update_username')
+def handle_username_update(data):
+    room = data['room']
+    new_username = data['username']
+    old_username = room_users[room].get(request.sid, 'Anonymous')
+    
+    room_users[room][request.sid] = new_username
+    
+    # Notify room about username change
+    emit('chat_message', {
+        'type': 'system',
+        'content': f'{old_username} changed their name to {new_username}'
+    }, room=room)
+    
+    # Send updated user list
+    emit('user_list_update', {
+        'users': list(room_users[room].values())
+    }, room=room)
 
 @socketio.on('video_event')
 def handle_video_event(data):
